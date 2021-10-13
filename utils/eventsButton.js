3d2fdc605612207da5730fde466ac08e9b35ca3a
Alexandre LAM <alexandrelam@outlook.com>
2021-10-13 22:08:47 +0200

    added better switch team

--- utils/eventsButton.js
+++ utils/eventsButton.js
@@ -1,13 +1,17 @@
-const Player = require("../game/Player");
+const { playerJoinTeam } = require("./helpers");
 
 module.exports.handleButton = async (interaction) => {
   if (!interaction.isButton()) return;
 
-  if (interaction.customId === "join") {
-    newPlayer = new Player(interaction.user.tag);
-    interaction.client.game.teamBlue.push(newPlayer);
-    interaction.reply({
-      content: `${interaction.user.tag} joined the game`,
-    });
+  if (interaction.customId === "join-blue") {
+    const teamBlue = interaction.client.game.teamBlue;
+    const teamRed = interaction.client.game.teamRed;
+    playerJoinTeam(interaction, teamBlue, teamRed, "blue");
+  }
+
+  if (interaction.customId === "join-red") {
+    const teamBlue = interaction.client.game.teamBlue;
+    const teamRed = interaction.client.game.teamRed;
+    playerJoinTeam(interaction, teamRed, teamBlue, "red");
   }
 };
