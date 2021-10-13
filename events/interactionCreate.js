d3d18adaeff9d3711386df3d25738710d053e7ec
Alexandre LAM <alexandrelam@outlook.com>
2021-10-13 20:33:13 +0200

    multiselect

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,23 +1,10 @@
+const { handleCommand } = require("../utils/eventsCommand");
+const { handleSelect } = require("../utils/eventsSelect");
+
 module.exports = {
   name: "interactionCreate",
   async execute(interaction) {
-    if (!interaction.isCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-      await command.execute(interaction);
-      console.log(
-        `${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`
-      );
-    } catch (error) {
-      console.error(error);
-      return interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
+    handleCommand(interaction);
+    handleSelect(interaction);
   },
 };
