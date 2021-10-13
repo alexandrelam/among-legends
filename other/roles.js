24cf7b1ff1b9f200ce63036d28bac5f6d08a1088
Alexandre LAM <alexandrelam@outlook.com>
2021-10-13 22:47:46 +0200

    added roles

---
+++ other/roles.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    type: "Crewmate",
+    name: "Taupe",
+    description:
+      "A la fin de la partie, avoir moins de participation (kills + assists) que tous les autres joueurs",
+    weight: 0.15,
+  },
+  {
+    type: "Crewmate",
+    name: "Faussaire",
+    description:
+      "A la fin de la partie, avoir plus de dégats et moins de morts que tous les autres joueurs",
+    weight: 0.2,
+  },
+  {
+    type: "Crewmate",
+    name: "Fermier",
+    description:
+      "A la fin de la partie, avoir tué plus de sbires  que tous les autres joueurs (si support: ce nombre est multiplié par 2)",
+    weight: 0.15,
+  },
+  {
+    type: "Crewmate",
+    name: "Inter",
+    description:
+      "A la fin de la partie, être mort plus de fois que tous les autres joueurs",
+    weight: 0.5,
+  },
+];
