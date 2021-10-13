bd30c8d35ce23e8ee753b53a1f9b579a302ea9ef
Alexandre LAM <alexandrelam@outlook.com>
2021-10-14 00:34:57 +0200

    tmp cos i go to sleep

---
+++ commands/vote.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageActionRow, MessageSelectMenu } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("vote")
+    .setDescription("Vote for the player you think is the imposter"),
+  async execute(interaction) {
+    const players = [
+      ...interaction.client.game.teamBlue,
+      ...interaction.client.game.teamRed,
+    ];
+
+    let choices = players.map((p) => ({ label: p.tag, value: p.tag }));
+    choices.push({ label: "Nobody", value: "nobody" });
+
+    const row = new MessageActionRow().addComponents(
+      new MessageSelectMenu()
+        .setCustomId("vote-imposter")
+        .setPlaceholder("Select someone")
+        .addOptions(choices)
+    );
+
+    await interaction.reply({
+      content: "Vote for the player you think is the imposter!",
+      components: [row],
+    });
+  },
+};
