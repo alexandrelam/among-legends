2bd6577aae34c7afe5c0d8d5540077f4fc8665d5
Alexandre LAM <alexandrelam@outlook.com>
2021-10-13 19:32:03 +0200

    added events dynamic

---
+++ events/interactionCreate.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: "interactionCreate",
+  async execute(interaction) {
+    if (!interaction.isCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+      console.log(
+        `${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`
+      );
+    } catch (error) {
+      console.error(error);
+      return interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  },
+};
--- index.js
+++ index.js
@@ -14,26 +14,17 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
-client.once("ready", () => {
-  console.log("Ready!");
-});
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    return interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
+}
 
 client.login(process.env.TOKEN);
