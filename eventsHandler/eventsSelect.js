7914e259eef35c6bd22ddd42eeec6da4b40c6a81
Alexandre LAM <alexandrelam@outlook.com>
2021-10-14 11:47:29 +0200

    clean up unused command

--- eventsHandler/eventsSelect.js
+++ eventsHandler/eventsSelect.js
@@ -1,13 +1,6 @@
 module.exports.handleSelect = async (interaction) => {
   if (!interaction.isSelectMenu()) return;
 
-  if (interaction.customId === "select") {
-    interaction.reply({
-      content: "Something was selected!",
-      ephemeral: true,
-    });
-  }
-
   if (interaction.customId === "vote-imposter") {
     if (interaction.values[0] === "nobody") {
       interaction.reply({ content: "You voted for nobody", ephemeral: true });
