ffddb2b5994874c13fd21ba3e77e60901150247d
Alexandre LAM <alexandrelam@outlook.com>
2021-10-15 12:08:24 +0200

    prettier more config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ['airbnb-base'],
-    parserOptions: {
-        ecmaVersion: 13,
-        sourceType: 'module',
-    },
-    rules: {},
+  env: {
+    browser: false,
+    es2021: true,
+  },
+  extends: ['prettier', 'airbnb-base'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {},
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-    trailingComma: 'es5',
-    tabWidth: 2,
-    semi: false,
-    singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
 }
