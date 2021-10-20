948937a41521505ae19503be96acc7628ce60bf0
Victor DELIEGE <victor.deliege@gmail.com>
2021-10-20 19:16:46 +0200

    add redteam + fix choices

--- commands/vote.js
+++ commands/vote.js
@@ -11,13 +11,35 @@ module.exports = {
       interaction.client.game.isBlueVoting ||
       interaction.client.game.isRedVoting
     ) {
-      const curr_player = getCurrentPlayer(interaction)
       const teamBlue = interaction.client.game.teamBlue
+      const teamRed = interaction.client.game.teamRed
       interaction.client.game.channel = getChannel(interaction)
 
       teamBlue.forEach((p) => {
         let choices = teamBlue
-          .filter((player) => player.tag !== curr_player.tag)
+          .filter((player) => p.tag !== player.tag)
+          .map((player) => ({
+            label: player.tag,
+            value: player.tag,
+          }))
+        choices.push({ label: 'Nobody', value: 'nobody' })
+
+        p.userInstance.send({
+          content: 'Vote for the player you think is the imposter!',
+          components: [
+            new MessageActionRow().addComponents(
+              new MessageSelectMenu()
+                .setCustomId('vote-imposter')
+                .setPlaceholder('Select someone')
+                .addOptions(choices)
+            ),
+          ],
+        })
+      })
+
+      teamRed.forEach((p) => {
+        let choices = teamRed
+          .filter((player) => p.tag !== player.tag)
           .map((player) => ({
             label: player.tag,
             value: player.tag,
